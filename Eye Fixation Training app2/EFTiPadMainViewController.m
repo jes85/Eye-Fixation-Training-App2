93749e5c54190d9da38d2975a89cf0df0a496d12
Jeremy Schreck <jes85@duke.edu>
2014-10-15 21:36:04 -0400

    Ready for github

--- Eye Fixation Training app2/EFTiPadMainViewController.m
+++ Eye Fixation Training app2/EFTiPadMainViewController.m
@@ -22,36 +22,6 @@
 
 @implementation EFTiPadMainViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-- (IBAction)backgroundTouched:(id)sender {
-}
-
-- (IBAction)startButtonPressed:(id)sender {
-}
-- (IBAction)directionsButtonTouched:(id)sender {
-}
-*/
 
 
 @end
